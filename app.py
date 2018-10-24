86198fb62990d8fb08ac950e076f716b18f1c13c
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-10-23 22:42:44 -0400

    Using bootstrap to create a nav bar

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask # Imports Flask
+from flask import Flask, render_template # Imports Flask and renders the template
 
 
 app = Flask(__name__) # Placeholder for app.py
 
 @app.route('/') # Route for our index page/Front page
 def index(): # Defines the function index
-    return 'INDEX5' # What pops up on the UI
+    return render_template('home.html') # Anything you put home.html will appear
 
 
 if __name__ == '__main__': # The script will be executed
