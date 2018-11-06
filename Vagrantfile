9db651b3fe53b3a21f861a993c03ae54c2a2290f
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-11-06 08:28:08 -0500

    some styles, created register section, created db

--- Vagrantfile
+++ Vagrantfile
@@ -22,10 +22,9 @@ Vagrant.configure("2") do |config|
     sudo service apache2 restart
 SCRIPT
 
-
     config.vm.box = "scotch/box"
     config.vm.network "private_network", ip: "192.168.33.10"
-    config.vm.network "forwarded_port", guest: 80, host: 8080
+    config.vm.network "forwarded_port", guest: 5000, host: 5000
     config.vm.hostname = "scotchbox"
     config.vm.synced_folder ".", "/var/www/public", :mount_options => ["dmode=777", "fmode=666"]
     config.vm.provision "shell", inline: $script
--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template # Imports Flask and renders the template
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging # Imports Flask and renders the template
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
 
 app = Flask(__name__) # Placeholder for app.py
 
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MYSQL
+mysql = MySQL(app)
 
 @app.route('/') # Route for our index page/Front page
 def index(): # Defines the function index
@@ -11,10 +23,45 @@ def index(): # Defines the function index
 def about():
     return render_template('about.html')
 
-@app.route('/register')
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.length(min=1, max=50)])
+    username = StringField('Username', [validators.length(min=1, max=25)])
+    email = StringField('Email', [validators.length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+        form = RegisterForm(request.form)
+        if request.method == 'POST' and form.validate():
+            name = form.name.data
+            email = form.email.data
+            username = form.username.data
+            password = sha256_crypt.encrypt(str(form.password.data))
+
+            # Crete cursor
+            cur = mysql.connection.cursor()
+
+            # Execute query
+            cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+            # Commit to DB
+            mysql.connection.commit()
+
+            # Close connection
+            cur.close()
+
+            flash('You are now registered and can log in', 'Success!')
+
+            return redirect(url_for('login'))
+        return render_template('register.html', form=form)
+
 
 if __name__ == '__main__': # The script will be executed
-   app.run(debug=True) # Runs the app / Debug mode means wont have to rerun to server to see changes 
+    app.secret_key='dsperezsilva171'
+    app.run(debug=True, host='0.0.0.0') # Runs the app / Debug mode means wont have to rerun to server to see changes 
 
